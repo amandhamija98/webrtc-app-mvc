9894cd6abe101b83abab5405a336f966d370efcc
amandhamija98 <amandhamija98@gmail.com>
2020-06-04 20:05:11 +0530

    set up project and home page

---
+++ VideoCallHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace webrtc_app
+{
+	public class VideoCallHub : Hub
+	{
+		public void Hello()
+		{
+			Clients.All.hello();
+		}
+	}
+}
